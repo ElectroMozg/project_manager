9603acf807cfc5144b80fcab38f61aa4d9e8c036
Oleg Rudchenko <kio5885@gmail.com>
2022-09-07 17:56:31 +0700

    реализован new

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,10 @@ fn main() {
     let altium_gerber_path = format!("{}/Outputs/Gerber", project_path);
     let altium_drill_path = format!("{}/Outputs/NC Drill", project_path);
     let docs_path = format!("{}/RustTest", project_path);
-    let name_project = find_name(&project_path);
+    //let name_project = find_name(&project_path);
 
-    copy_gerbers(&altium_gerber_path, &docs_path, &name_project);
-    copy_drills(&altium_drill_path, &docs_path, &name_project);
+    //copy_gerbers(&altium_gerber_path, &docs_path, &name_project);
+    //copy_drills(&altium_drill_path, &docs_path, &name_project);
 }
 
 fn copy_gerbers(from: &str, to: &str, name: &str) {
@@ -84,20 +84,45 @@ fn copy_drills(from: &str, to: &str, name: &str) {
     .unwrap();
 }
 
+struct Gerber {
+    path_from: String,
+    path_to: String,
+    project_name: String
+}
+
+impl Gerber {
+    
+    fn new(path_from: String, path_to: String) -> Gerber {
+        let project_name: String = Self::find_name(&path_from);
+        return Gerber{path_from, path_to, project_name};
+    }
+
+    pub fn copy_gerbers(&self) {}
+
+    pub fn copy_drills(&self) {
+        fs::copy(
+            gerber_path(&self.path_from, &self.project_name, ".TXT"),
+            gerber_path(&self.path_to, &self.project_name, ".TXT"),
+        )
+        .unwrap();
+    }
+
+    fn find_name(project_path: &str) -> String {
+        let mut name = "None".to_string();
+    
+        for file in fs::read_dir(project_path).unwrap() {
+            let file_name = file.unwrap().file_name().into_string().unwrap();
+    
+            if file_name.contains(".PrjPcb") {
+                name = file_name.split('.').next().unwrap().to_string();
+            }
+        }
+        return name.to_string();
+    }
+}
+
 fn gerber_path(folder: &str, project_name: &str, extension: &str) -> String {
     format!("{}/{}{}", folder, project_name, extension)
 }
 
-fn find_name(project_path: &str) -> String {
 
-    let mut name = "None".to_string();
-
-    for file in fs::read_dir(project_path).unwrap() {
-        let file_name = file.unwrap().file_name().into_string().unwrap();
-
-        if file_name.contains(".PrjPcb") {
-            name = file_name.split('.').next().unwrap().to_string();
-        }
-    }
-    return name.to_string();
-}
